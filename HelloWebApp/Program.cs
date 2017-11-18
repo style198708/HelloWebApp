2be39764234ce0f89eddbe4022215b4061e1fade
Tonky <Administrator@TONKY-PC>
2017-11-18 21:17:23 +0800

    1

--- HelloWebApp/Program.cs
+++ HelloWebApp/Program.cs
@@ -19,6 +19,8 @@ namespace HelloWebApp
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
+                .UseUrls("http://*:5000")
                 .UseStartup<Startup>()
                 .Build();
     }
